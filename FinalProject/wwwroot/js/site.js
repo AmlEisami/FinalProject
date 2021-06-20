cb5ca90788fc23287a6a8e541029265e47522783
Tovad <tovtov989@gmail.com>
2021-06-20 21:08:00 +0300

    Adding map and add button

--- FinalProject/wwwroot/js/site.js
+++ FinalProject/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function openMap(url) {
+    document.getElementById("urlIframe").setAttribute('src', 'https://www.google.com/maps/embed?q=!1m18!1m12!1m3!1d216747.92044885!2d35.00251835!3d31.915120850000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x250a9790b78fdae2!2sModi&#39;in%20Maccabim%20Re&#39;ut%20Municipality!5e0!3m2!1sen!2sil!4v1624211301617!5m2!1sen!2sil')
+}
