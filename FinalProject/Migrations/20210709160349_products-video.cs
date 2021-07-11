269aa4a7705815b2a8cb2b65190bad5c64c0f7db
Naor Nagar <naornagar10@gmail.com>
2021-07-11 19:54:07 +0300

    add

---
+++ FinalProject/Migrations/20210709160349_products-video.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FinalProject.Migrations
+{
+    public partial class productsvideo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Video",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Video",
+                table: "Products");
+        }
+    }
+}
