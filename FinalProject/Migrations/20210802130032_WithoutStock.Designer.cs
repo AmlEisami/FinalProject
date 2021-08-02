8519b4a8a7a253b7cdaaaaad9272926f470c3e23
Naor Nagar <naornagar10@gmail.com>
2021-08-02 22:59:30 +0300

    add css

--- FinalProject/Migrations/20210802130032_WithoutStock.Designer.cs
+++ FinalProject/Migrations/20210802130032_WithoutStock.Designer.cs
@@ -4,14 +4,16 @@ using FinalProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FinalProject.Migrations
 {
     [DbContext(typeof(FinalProjectContext))]
-    partial class FinalProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20210802130032_WithoutStock")]
+    partial class WithoutStock
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FinalProject/Migrations/20210802130032_WithoutStock.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FinalProject.Migrations
+{
+    public partial class WithoutStock : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Stock",
+                table: "Products");
+        }
+    }
+}
