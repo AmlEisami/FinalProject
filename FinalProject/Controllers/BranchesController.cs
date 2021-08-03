db065efecc6a34eb8116bfc41217aa3e17ebe89e
Tova <Tovtov989@gmail.com>
2021-08-03 23:49:52 +0300

    cleaning branch code

--- FinalProject/Controllers/BranchesController.cs
+++ FinalProject/Controllers/BranchesController.cs
@@ -26,7 +26,7 @@ namespace FinalProject.Controllers
             return View(await finalProjectContext.ToListAsync());
         }
 
-       
+
         // GET: Branches/Create
         public IActionResult Create()
         {
@@ -51,92 +51,5 @@ namespace FinalProject.Controllers
             return View(branches);
         }
 
-        // GET: Branches/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var branches = await _context.Branch.FindAsync(id);
-            if (branches == null)
-            {
-                return NotFound();
-            }
-            ViewData["UsersId"] = new SelectList(_context.Users, "Id", "Fullname", branches.UsersId);
-            return View(branches);
-        }
-
-        // POST: Branches/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,BranchName,Location,UsersId")] Branches branches)
-        {
-            if (id != branches.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(branches);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!BranchesExists(branches.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["UsersId"] = new SelectList(_context.Users, "Id", "DisplayName", branches.UsersId);
-            return View(branches);
-        }
-
-        // GET: Branches/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var branches = await _context.Branch
-                .Include(b => b.BranchManager)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (branches == null)
-            {
-                return NotFound();
-            }
-
-            return View(branches);
-        }
-
-        // POST: Branches/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var branches = await _context.Branch.FindAsync(id);
-            _context.Branch.Remove(branches);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool BranchesExists(int id)
-        {
-            return _context.Branch.Any(e => e.Id == id);
-        }
     }
 }
